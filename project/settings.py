b175b46f38f45e076ab428d89f69bce430986433
FloWambui <gflorencewambui@gmail.com>
2022-07-04 09:32:16 +0300

    ft: add the accounts login form

--- project/settings.py
+++ project/settings.py
@@ -57,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    'accounts.middleware.AccountCheckMiddleWare',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -142,6 +145,7 @@ cloudinary.config(
 )
 
 # Email configurations remember to install python-decouple
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
