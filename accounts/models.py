085834bf48a9f2195d54372242f5b770ecf5ecc7
gladyswambura <gladyswahito7@gmail.com>
2022-07-05 14:46:52 +0300

    Creates the Api endpoints

--- accounts/models.py
+++ accounts/models.py
@@ -18,30 +18,33 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
+        # extra_fields.setdefault("is_staff", False)
+        # extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("user_type", 1)
-        extra_fields.setdefault("last_name", "System")
-        extra_fields.setdefault("first_name", "Administrator")
+    def create_superuser(self, email,password=None, **extra_fields):
+        # extra_fields.setdefault("is_staff", True)
+        # extra_fields.setdefault("is_superuser", True)
+        # extra_fields.setdefault("user_type", 1)
+        # extra_fields.setdefault("last_name", "System")
+        # extra_fields.setdefault("first_name", "Administrator")
 
-        assert extra_fields["is_staff"]
-        assert extra_fields["is_superuser"]
+        # assert extra_fields["is_staff"]
+        # assert extra_fields["is_superuser"]
         return self._create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser):
     USER_TYPE = ((1, "Admin"), (2, "Voter"))
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     username = None  # Removed username, using email instead
     email = models.EmailField(unique=True)
     user_type = models.CharField(default=2, choices=USER_TYPE, max_length=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    USERNAME_FIELD = "email"
+    
+    USERNAME_FIELD = 'email'    # username field is email
     REQUIRED_FIELDS = []
     objects = UserManager()
 
